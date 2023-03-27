66d9d9fc68cabada91843eb95740ec6add931542
Bruno Mendes <brpimendes@gmail.com>
2023-03-27 17:12:38 -0300

    ajustes no layout da pagina de listagem

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,5 +1,58 @@
-const List = () => {
-    return <h1>Lista</h1>
-}
+import { ButtonsContainer, ListContainer, ListTable } from "./styles";
+import { Trash } from "@phosphor-icons/react";
 
-export default List
+const List = () => {
+  return (
+    <ListContainer>
+      <h1>Crie sua lista</h1>
+      <ButtonsContainer>
+        <button>Adicionar ítens</button>
+        <button>Salvar Lista</button>
+      </ButtonsContainer>
+      <ListTable>
+        <table>
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Quantidade</th>
+              <th>Valor unitário</th>
+              <th>Total</th>
+              <th>Ação</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Pão</td>
+              <td>1</td>
+              <td>8.5</td>
+              <td>8.5</td>
+              <td>
+                <button>{<Trash size={24} />}</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Pão</td>
+              <td>1</td>
+              <td>8.5</td>
+              <td>8.5</td>
+              <td>
+                <button>{<Trash size={24} />}</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Pão</td>
+              <td>1</td>
+              <td>8.5</td>
+              <td>8.5</td>
+              <td>
+                <button>{<Trash size={24} />}</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </ListTable>
+    </ListContainer>
+  );
+};
+
+export default List;
---
+++ src/pages/List/styles.ts
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.main`
+  height: 80vh;
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  h1 {
+    margin-top: 1rem;
+    font-size: 2.5rem;
+    text-align: center;
+  }
+`;
+
+export const ListTable = styled.div`
+  flex: 1;
+  margin-top: 1rem;
+  overflow: auto;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background: #d70034;
+      padding: 1rem 0;
+      text-align: left;
+      color: #fff;
+      font-size: 1.5rem;
+      line-height: 1.2;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: #e1e1e6;
+      border-top: 4px solid #fff;
+      padding: 1rem;
+      font-size: 1.2rem;
+      line-height: 1.2;
+
+      &:first-child {
+        width: 50%;
+        padding: 1.5rem;
+      }
+
+      &:last-child {
+        padding: 1.5rem;
+      }
+
+      button {
+        background-color: transparent;
+        border: 0;
+        width: 44px;
+        height: 44px;
+        border-radius: 50%;
+        transition: all 0.333s ease;
+
+        &:hover {
+          background-color: #d70034;
+          color: #fff;
+        }
+      }
+    }
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+
+  button {
+    background-color: #d70034;
+    padding: 2rem 4rem;
+    border: none;
+    border-radius: 8px;
+    color: #fff;
+    font-size: 1.2rem;
+    transition: all 0.333s ease;
+
+    &:hover {
+      background-color: #910023;
+    }
+  }
+`;
