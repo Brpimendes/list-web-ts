e7ef01d6f9ef19bd620f02b37b8653ea0e230f3e
bruno.pimentel <bruno.pimentel@sonda.com>
2024-04-05 11:52:33 -0300

    feat: commit inicial para inclusao da pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,22 @@
-const Login = () => {
-    return <h1>Login</h1>
-}
+import {LoginContainer, LoginForm} from "./styles";
 
-export default Login
+const Login = () => {
+  return (
+    <LoginContainer>
+      <h1>Login</h1>
+      <LoginForm>
+        <label>
+          <span>Email:</span>
+          <input type="email" name="" id="" />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input type="password" name="" id="" />
+        </label>
+        <button type="submit">Enviar</button>
+      </LoginForm>
+    </LoginContainer>
+  );
+};
+
+export default Login;
